9c67ffee609fb8001148b790ba68ea077fc94fd7
Cong Minh <titcongminh21082004@gmail.com>
2023-01-31 14:40:06 +0700

    commit 31/1

--- WebBanGiayMVC/DataAccess/DanhMucDA.cs
+++ WebBanGiayMVC/DataAccess/DanhMucDA.cs
@@ -1,11 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using WebBanGiayMVC.Business;
+using WebBanGiayMVC.Models;
+using Dapper;
+using System.Data;
 
 namespace WebBanGiayMVC.DataAccess
 {
     public class DanhMucDA
     {
+        private string cs = ConfigurationManager.ConnectionStrings["Model_Context1"].ConnectionString;
+        private Model_Context _context = new Model_Context();
+        public List<DanhMuc> GetAllDanhMucs(out int total,string keyword = "", int pageIndex = 1,int pageSize = 10) 
+        {
+            using (var conn = new SqlConnection(cs))
+            {
+                conn.Open();              
+
+                //Add param
+                DynamicParameters dp = new DynamicParameters();
+                dp.Add("keyword", keyword);
+                dp.Add("pageIndex", pageIndex);
+                dp.Add("pageSize", pageSize);
+                dp.Add("total", dbType: DbType.Int32, direction: ParameterDirection.Output);
+                conn.Close();
+
+                var result = conn.Query<DanhMuc>("web_FilterDanhMuc", dp, commandType: CommandType.StoredProcedure).ToList();
+                total = dp.Get<int>("total");
+                conn.Close();
+                return result;
+            }
+        }
+
+        public List<DanhMuc> GetAllDanhMucByEF(out int total, string keyword = "", int pageIndex = 1, int pageSize = 10)
+        {
+            var result = _context.DanhMucs.AsQueryable();
+            if (string.IsNullOrEmpty(keyword))
+            {
+                result = result.Where(r => r.TenDanhMuc.ToLower().Contains(keyword.ToLower()));
+            }
+            total = result.Count();
+            if(pageIndex >= 1 && pageSize > 0)
+            {
+                result = result.OrderBy(r => r.Id).Skip((pageIndex - 1) * pageSize).Take(pageSize);
+            }
+            return result.ToList();
+        }
+
+
+
+
     }
 }
---
+++ WebBanGiayMVC/Service/DanhMuc/IDanhMucService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebBanGiayMVC.DataAccess;
+using WebBanGiayMVC.Models;
+
+namespace WebBanGiayMVC.Business
+{
+    internal interface IDanhMucService
+    {
+        List<DanhMuc> GetAllDanhMucs();
+    }
+
+    public class DanhMucSerVice : IDanhMucService
+    {
+        DanhMucDA _danhMucDA = new DanhMucDA();
+        public List<DanhMuc> GetAllDanhMucs()
+        {
+            var total = 0;
+            return _danhMucDA.GetAllDanhMucs(out total);
+            
+        }
+
+    }
+}
