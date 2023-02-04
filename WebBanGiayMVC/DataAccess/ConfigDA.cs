aff1499c6f046da7e8ac13c80ca218b5b2a2899b
Cong Anh <conganhforwork@gmail.com>
2023-02-04 21:37:51 +0700

    update code ngay 4/2

--- WebBanGiayMVC/DataAccess/ConfigDA.cs
+++ WebBanGiayMVC/DataAccess/ConfigDA.cs
@@ -1,11 +1,45 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
+using WebBanGiayMVC.Models;
 
 namespace WebBanGiayMVC.DataAccess
 {
     public class ConfigDA
     {
+        private string cs = ConfigurationManager.ConnectionStrings["Model_Context1"].ConnectionString;
+        private Model_Context _context = new Model_Context();
+        public CauHinh GetCauHinhByMaCauHinh(string code)
+        {
+            try
+            {
+                using (var conn = new SqlConnection(cs))
+                {
+                    var storeName = "GetCauHinhByMaCauHinh";//ten proc
+
+                    //Add param
+                    DynamicParameters dp = new DynamicParameters();
+                    dp.Add("code", code);
+                    conn.Open();
+                    var result = conn.QueryFirst<CauHinh>(storeName, dp, commandType: System.Data.CommandType.StoredProcedure);
+                    conn.Close();
+
+                    return result;
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+                return null;
+            }
+
+           
+        }
     }
 }
