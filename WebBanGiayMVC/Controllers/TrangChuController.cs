c3111dbfe11efbfa1157f57ddb9deca82dcc0e5d
Cong Minh <titcongminh21082004@gmail.com>
2023-02-07 09:09:01 +0700

    commit 2/7

---
+++ WebBanGiayMVC/Controllers/TrangChuController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanGiayMVC.Controllers
+{
+    public class TrangChuController : Controller
+    {
+        // GET: TrangChu
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Add_To_Wishlist()
+        {
+            return View();
+        }
+        public ActionResult Cart()
+        {
+            return View();
+        }
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Men()
+        {
+            return View();
+        }
+        public ActionResult Women()
+        {
+            return View();
+        }
+        public ActionResult Order_Complete()
+        {
+            return View();
+        }
+        public ActionResult Product_Detail()
+        {
+            return View();
+        }
+    }
+}
